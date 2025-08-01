39ffc8024b7780d1ecd152dd0dae3b1325970e67
harshithadakarapu28 <harshithadakarapu@gmail.com>
2025-08-01 13:08:33 +0530

    Create PyramidPattern.py

---
+++ PyramidPattern.py
@@ -0,0 +1,8 @@
+"""Pyramid Pattern in Python"""
+n = int(input())
+for i in range(1, n+1):
+    for j in range(n-i):
+        print(" ", end = " ")
+    for k in range(1, i*2):
+        print("*", end = " ")
+    print()
